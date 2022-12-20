91f1e4e889957c0bc4ba99ce38457dd1c08996fb
unknown <isroilovabduraxmon77@gmail.com>
2022-12-20 07:23:02 +0500

    first commit

---
+++ dars.py
@@ -0,0 +1,15 @@
+import time
+a = int(input("codni kirit: "))
+
+if a == 8888:
+    for x in range(11):
+        time.sleep(1)
+        print(x , "secund")  
+        print("dasturga kirdinggiz")
+
+else:
+ for x in range(5392374652837653874562356932874692346326487693487):
+    time.sleep(1)
+    print(x , "secund", "XATO")   
+   
+
---
+++ dastur.py
@@ -0,0 +1,1 @@
+from dars import *
